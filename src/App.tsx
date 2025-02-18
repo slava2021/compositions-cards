401a4a173e7a24ec989a0383ee8cc5b893869de0
Vyacheslav <asik2006@mail.ru>
2025-02-18 23:25:39 +0300

    End task

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { CardItems } from "./components/CardItems";
+import { CardImage } from "./components/CardImage";
+import "./App.scss";
+
+function App() {
+  const propsCard = {
+    title: "Card title",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  };
+
+  return (
+    <>
+      <CardItems
+        title={propsCard.title}
+        text={propsCard.text}
+        image={<CardImage />}
+      />
+      <CardItems title={propsCard.title} text={propsCard.text} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CardItems.tsx
@@ -0,0 +1,22 @@
+import { React } from "react";
+// import { CardImage } from "./CardImage";
+
+type CardsProps = {
+  title: string;
+  text: string;
+  className: string;
+  image?: string;
+};
+
+export const CardItems = ({ title, text, image }): CardsProps => (
+  <div className="card" style={{ width: "18rem" }}>
+    {image}
+    <div className="card-body">
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </div>
+  </div>
+);
